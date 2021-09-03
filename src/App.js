8d6a08d849aae9fbe14b6f71536242fd78435a87
exit <bhupendranagda99@gmail.com>
2021-09-03 20:27:36 +0530

    Add Features

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
-import './App.css';
-import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm'
+import { useState } from "react";
+import "./App.css";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark Mode Has been Enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Dark Mode Has been Enabled", "success");
+      document.title = "TextUtils - Light Mode";
+    }
+  };
+
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils" />
-      <div className="container">
-        {/* <TextForm heading="Enter the text to analyze" /> */}
-        <About/>
-      </div>
+      <Router>
+        <Navbar
+          toggleMode={toggleMode}
+          mode={mode}
+          title="TextUtils"
+          aboutText="About TextUtils"
+        />
+        <Alert alert={alert} />
+        <div className="container">
+          <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/">
+              <TextForm mode={mode} heading="Enter the text to analyze" />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,38 +1,93 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
 
-    const handleUpClick=()=>{
-        let newText = text.toUpperCase()
-        setText(newText)
-    }
-    const handleLoClick=()=>{
-        let newText = text.toLowerCase()
-        setText(newText)
-    }
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
 
-    const handleOnChange =(e)=>{
-        setText(e.target.value)
-    }
+  const handleClearText = (e) => {
+    setText("");
+  };
 
-    const [text,setText] = useState('');
-    return (
-        <>
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="mb-3 my-3">
-                <textarea onChange={handleOnChange} value={text} className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
-                <button onClick={handleUpClick} className="btn btn-primary mx-1">Convert To Uppercase</button>
-                <button onClick={handleLoClick} className="btn btn-primary mx-1">Convert To Lowercase</button>
-            </div>
+  const handleCopy = () => {
+    let wholeText = document.getElementById("myBox");
+    wholeText.select();
+    navigator.clipboard.writeText(wholeText.value);
+  };
+
+  const handleExtraSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div>
+        <h1 style={{ color: props.mode === "light" ? "black" : "white" }}>
+          {props.heading}
+        </h1>
+        <div className="mb-3 my-3">
+          <textarea
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "grey",
+              color: props.mode === "light" ? "black" : "white",
+            }}
+            onChange={handleOnChange}
+            value={text}
+            className="form-control"
+            id="myBox"
+            rows="8"
+          ></textarea>
+          <button onClick={handleUpClick} className="my-3 btn btn-primary mx-1">
+            Convert To Uppercase
+          </button>
+          <button onClick={handleLoClick} className="my-3 btn btn-primary mx-1">
+            Convert To Lowercase
+          </button>
+          <button
+            onClick={handleClearText}
+            className="my-3 btn btn-primary mx-1"
+          >
+            Clear Text
+          </button>
+          <button onClick={handleCopy} className="my-3 btn btn-primary mx-1">
+            Copy Text
+          </button>
+          <button
+            onClick={handleExtraSpace}
+            className="my-3 btn btn-primary mx-1"
+          >
+            Remove Extra Space
+          </button>
         </div>
-        <div>
-           <h1>Text Summary</h1> 
-           <p>{text.split(" ").length} words and {text.length} letter</p>
-           <p>{ 0.008 * text.split(" ").length} Minutes read</p>
-           <h2>Preview</h2>
-           <p>{text}</p>
-        </div>
-        </>
-    )
+      </div>
+      <div>
+        <h1 style={{ color: `${props.mode === "light" ? "black" : "white"}` }}>
+          Text Summary
+        </h1>
+        <p style={{ color: `${props.mode === "light" ? "black" : "white"}` }}>
+          {text.split(" ").length} words and {text.length} letter
+        </p>
+        <p style={{ color: `${props.mode === "light" ? "black" : "white"}` }}>
+          {0.008 * text.split(" ").length} Minutes read
+        </p>
+        <h2 style={{ color: `${props.mode === "light" ? "black" : "white"}` }}>
+          Preview
+        </h2>
+        <p style={{ color: `${props.mode === "light" ? "black" : "white"}` }}>
+          {text}
+        </p>
+      </div>
+    </>
+  );
 }
